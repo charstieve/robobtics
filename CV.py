64a1de84dc8fcacd434e1702df2b74f711ebc4ff
charstieve <charlotteolivia930@gmail.com>
2025-04-02 15:37:21 -0400

    New ball recognition code

--- CV.py
+++ CV.py
@@ -33,47 +33,66 @@ class BallTracker:
         return self._detector.detectMarkers(gray)
      
     #Find in pixels
-    def find_orange_ball_pixels(self, frame) -> tuple[float, float] | None:
+    # TODO: Currently sends the frame back with the highlight. The pixel coordinates of the balls
+    # are also generated here, just have to figure out how to return them along with the frame when 
+    # they are sometimes None, which seems to cause an issue when you try to grab the frame 
+    # at the bottom where the webcam is run
+    def find_orange_ball_pixels(self, frame): #-> tuple[float, float] | None:
         '''
         Finds the orange ball position in the frame using cv2 and hue circle filter to find the largest orange circle in the frame
         '''
-        # img = frame.copy()
-        
 
         # #Only take orange from the frame
-        # lower_orange = np.array([10, 100, 100])
-        # upper_orange = np.array([25, 255, 255])
-        # mask = cv2.inRange(frame, lower_orange, upper_orange)
+        hsv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # https://stackoverflow.com/questions/48528754/what-are-recommended-color-spaces-for-detecting-orange-color-in-open-cv
+        # Lower range (darker orange) values
+        lower_orange = np.array([10, 50, 70])
+        # Upper range (lighter orange) values
+        upper_orange = np.array([35, 255, 255])
 
-        gray_scale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Apply a mask that removes any color not within the orange range
+        # It sets everything that is not orange to black and anything that is orange 
+        # to white
+        mask = cv2.inRange(hsv_image, lower_orange, upper_orange)
+
+        # Apply the mask to the frame, so anything that is white keeps its color
+        # and anything that was previously turned black stays black
+        masked_frame = cv2.bitwise_and(frame, frame, mask=mask)
+
+        # Convert to greyscale with the image, so the orange ping pong balls will be turned
+        # into greyscale so that they can be used with Hough Circles
+        gray_scale_frame = cv2.cvtColor(masked_frame, cv2.COLOR_BGR2GRAY)
         
+        # Apply a Gaussian Blur to the grey-scaled fram for use with Hough Circles
         blurred_gray_frame = cv2.GaussianBlur(gray_scale_frame, ksize=(7,7), sigmaX=0)
         
-        balls_in_frame = cv2.HoughCircles(blurred_gray_frame, cv2.HOUGH_GRADIENT, 1.8, 100)
+        # Find the coordinates of the balls in the frames using Hough Circles
+        balls_in_frame = cv2.HoughCircles(blurred_gray_frame, cv2.HOUGH_GRADIENT, 1.8, 25)
 
-        cimg = cv2.cvtColor(blurred_gray_frame, cv2.COLOR_GRAY2BGR)
+        # Test statement to show when the balls are being detected in case it is not shown 
+        # visually
         print('_______')
         print(balls_in_frame)
         print('_______')
+
+        # If there are no balls in the frame then return the frame normally
         if balls_in_frame is None:
-            return
-        # for ball in balls_in_frame[0]:
-        #     print("ball: ", ball)
-        # # return
+            return frame
+
+        # Iterate through every ball in the frame
         for ball in balls_in_frame[0]:
-        #     # print(ball)
-        #     #Draw circle around ball
-        #     print('----------------')
-        #     print(ball[0], ball[1])
-            cv2.circle(cimg, (int(ball[0]), int(ball[1])), int(ball[2]), (0,0,255), 2)
+
+            #Draw circle around ball
+            cv2.circle(frame, (int(ball[0]), int(ball[1])), int(ball[2]), (0,0,255), 2)
             
-        #     #Draw circle in center
-            cv2.circle(cimg, (int(ball[0]), int(ball[1])), 2, (0,0,255), 3)
+            #Draw circle in center of the ball
+            cv2.circle(frame, (int(ball[0]), int(ball[1])), 2, (0,0,255), 3)
 
-            cv2.imshow('detected circles',cimg)
-
+        # These are the coordinates that should also be returned, or can be returned in 
+        # another function
         ball_tuple = (ball[0])
-        return ball_tuple
+        # Return the frame with the circles drawn around the balls.
+        return frame
         
     #Convert to meters and take into account april tag to find the position of the ball for the robot
     def find_ball_position(self, frame) -> tuple[float, float] | None:
@@ -83,17 +102,46 @@ class BallTracker:
         
         ball_position = self._find_ball_position(frame)
 
-    
-while True:
-    cap = cv2.VideoCapture(0)
-    ret, frame = cap.read()
+# https://stackoverflow.com/questions/2601194/displaying-a-webcam-feed-using-opencv-and-python
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(1)
+
+if vc.isOpened(): # try to get the first frame
+    ret, frame = vc.read()
+else:
+    ret = False
+
+while ret:
+
+    ret, frame = vc.read()
+
     ball_tracker = BallTracker(tag_size=0.05, robot_tag_offset_x=0.1, robot_tag_offset_y=0.1, apriltag_family="tag36h11", camera_number=1)
-    ball_tracker.find_orange_ball_pixels(frame)
-    # frame = cv2.imread("apriltag_36h11.png")
-    # tag = ball_tracker.april_tag_corners(frame)
-    # print(tag)
-    cv2.imshow("Frame", frame)
-    cv2.waitKey(1)
+
+    frame = ball_tracker.find_orange_ball_pixels(frame)
+
+    cv2.imshow("preview", frame)
+
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+cv2.destroyWindow("preview")
+vc.release()
+
+# cap = cv2.VideoCapture(1)
+# while True:
+#     #cap = cv2.VideoCapture(1)
+#     ret, frame = cap.read()
+    # ball_tracker = BallTracker(tag_size=0.05, robot_tag_offset_x=0.1, robot_tag_offset_y=0.1, apriltag_family="tag36h11", camera_number=1)
+    # ball_tracker.find_orange_ball_pixels(frame)
+#     # frame = cv2.imread("apriltag_36h11.png")
+#     # tag = ball_tracker.april_tag_corners(frame)
+#     # print(tag)
+#     cv2.imshow("Frame", frame)
+#     cv2.waitKey(1)
+
+# cap.release()  # Release camera when done
+# cv2.destroyAllWindows()  # Close all OpenCV windows
 
             
             
