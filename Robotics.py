53bf2367382a785152b7a04280df66973747b290
UckTay <56901754+UckTay@users.noreply.github.com>
2025-04-09 16:00:36 -0400

    inverted threading

--- Robotics.py
+++ Robotics.py
@@ -15,18 +15,32 @@ def set_gripper(bot, closed: bool):
     else:
         bot.gripper.release()
 
-def camera_thread_func(cam, ball_detection):
+def robot_thread_func(bot):
+    tag_offset_x = 0.0
+    tag_offset_y = 5 * 0.0254
+    z = 0.06
     while True:
-        ret, frame = cam.read()
-        if not ret:
-            print("Failed to capture frame")
+        if ball_position == (None, None, None) or ball_position == None:
+            print("No ball detected")
             continue
-        ball_position = ball_detection.get_ball_position(frame)
-        cv2.imshow("Ball Detection", frame)
-        cv2.waitKey(1)
+        x, y, z = ball_position
+        print(f"Ball position: {ball_position}")
+        print(-x, y + tag_offset_y, 0.06)
+
+        bot.arm.set_ee_pose_components(y=-x, x=y - tag_offset_y, z=0.06)
+
+        bot.arm.set_ee_pose_components(y=-x, x=y - 0.07 - tag_offset_y, z=0.06)
+        bot.arm.set_trajectory_time(moving_time=0.5)
+        hit_distance = 0.05  # The distance to move forwards so that the arm hits the ball
+        bot.arm.set_ee_pose_components(y=-x, x=y - tag_offset_y + hit_distance, z=0.06)
+        bot.arm.set_trajectory_time(moving_time=1)
+        bot.arm.go_to_home_pose()
+        time.sleep(1)
+
+
     
 def main():
-
+    global ball_position
     bot = InterbotixManipulatorXS(
         robot_model='rx200',
         group_name='arm',
@@ -42,47 +56,22 @@ def main():
     bot.arm.go_to_home_pose()
     ball_detection = BallDetection()
 
-    tag_offset_x = 0.0
-    tag_offset_y = 5 * 0.0254
-    z = 0.06
-    previous_position = None
 
-    cam_thread = threading.Thread(target=camera_thread_func, args=(cam, ball_detection))
-    cam_thread.start()
+    bot_thread = threading.Thread(target=robot_thread_func, args=(bot))
+    bot_thread.start()
 
     while True:
-        try:
-            # for _ in range(50):
-            #     _, _ = cam.read()
-            
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to capture frame")
+            continue
+        ball_position = ball_detection.get_position(frame)
+        print("Ball position is: ", ball_position)
+        cv2.imshow("Ball Detection", frame)
+        cv2.waitKey(1)
 
-            if ball_position == (None, None, None) or ball_position == None:
-                print("No ball detected")
-                continue
-            x, y, z = ball_position
-            print(f"Ball position: {ball_position}")
-            print(-x, y + tag_offset_y, 0.06)
-            # y=-x, x= y + tag_offset_y, z=0.06
-            # Go to ball
-            # user_input = input("Press Enter to continue...")
-            
-            print(ball_position)
-            # if user_input.lower() == 'q':
-            #     print("Exiting...")
-            #     cam.release()
-            #     cv2.destroyAllWindows()
-            #     robot_shutdown()
-            #     break
-            bot.arm.set_ee_pose_components(y=-x, x= y - tag_offset_y, z=0.06)
 
-            # bot.arm.set_ee_pose_components(y=-x, x= y - 0.07 - tag_offset_y, z=0.06)
-            bot.arm.set_trajectory_time(moving_time=0.5)
-            # hit_distance = 0.05 # The distance to move forwards so that the arm hits the ball
-            # bot.arm.set_ee_pose_components(y=-x, x=y - tag_offset_y + hit_distance, z=0.06)
-            # bot.arm.set_trajectory_time(moving_time=1)
-            # bot.arm.go_to_home_pose()
-        except Exception as e:
-            print(e)
+
     print('Terminating ...')
     # while True:
     #     x = input("X")
@@ -94,7 +83,7 @@ def main():
 
     cam.release()
     cv2.destroyAllWindows()
-    robot_shutdown()
+    # robot_shutdown()
 
 
 if __name__ == '__main__':
