cb84dac06a2bac7940a69d80fec463b41c9e6256
Jay-Atal <thejayatal@gmail.com>
2025-04-13 21:47:59 -0400

    Added ball angle following.

--- Robotics.py
+++ Robotics.py
@@ -1,3 +1,4 @@
+import math
 import sys
 from interbotix_common_modules.common_robot.robot import robot_shutdown, robot_startup
 from interbotix_xs_modules.xs_robot.arm import InterbotixManipulatorXS
@@ -17,9 +18,14 @@ def set_gripper(bot, closed: bool):
         bot.gripper.release()
 
 def robot_thread_func(bot):
+    LOWER = 3 * 0.0254
+    UPPER = 12 * 0.0254
+    HOME_DISTANCE = 7 * 0.0254
     tag_offset_x = 0.0
     tag_offset_y = 5 * 0.0254
+    hit_distance = 0.05  # The distance to move forwards so that the arm hits the ball
     z = 0.06
+    previous_angle = None
     while True:
         if paused:
             continue
@@ -28,16 +34,34 @@ def robot_thread_func(bot):
             if ball_position == (None, None, None) or ball_position == None:
                 print("No ball detected")
                 continue
+
             x, y, z = ball_position
+            ax = y - tag_offset_y
+            ay = -x
+            az = 0.07
+
             print(f"Ball position thread: {ball_position}")
             print(-x, y + tag_offset_y, 0.06)
+            angle = math.atan(x/y)
+            distance = math.sqrt((ax ** 2) + (ay ** 2))
 
-            bot.arm.set_ee_pose_components(y = -x, x = y - tag_offset_y, z=0.07)
+            if LOWER < distance < UPPER:
+                bot.arm.set_ee_pose_components(x=ax, y=ay, z=az)
+                time.sleep(0.3)
+                bot.arm.set_ee_pose_components(x=ax + hit_distance, y=ay, z=az)
+                time.sleep(10)
+                previous_angle = None
+
+            if previous_angle == angle:
+                continue
+
+            # Lowerbound coordinates
+            hx = HOME_DISTANCE * math.sin(angle)
+            hy = HOME_DISTANCE * math.cos(angle)
+
+            bot.arm.set_ee_pose_components(x=hx, y=hy, z=az)
+
             print("moved to hit ball")
-            #bot.arm.set_trajectory_time(moving_time=0.3)
-            hit_distance = 0.05 # The distance to move forwards so that the arm hits the ball
-            #bot.arm.set_ee_pose_components(y = -x, x = y - tag_offset_y + hit_distance, z=0.07)
-            #time.sleep(1)
         except Exception as e:
             print(f"Error in robot thread: {e}")
             continue
@@ -57,6 +81,8 @@ def main():
     bot.arm.set_trajectory_time(moving_time=0.5)
     # bot.arm.go_to_sleep_pose()
     bot.arm.go_to_home_pose()
+    LOWER = 3 * 0.0254
+    bot.arm.set_ee_pose_components(x=0, y=LOWER, z=0.07)
     ball_detection = BallDetection()
 
     
